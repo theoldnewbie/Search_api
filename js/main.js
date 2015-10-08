5d873995ba4e0dd2d8ffe6cdeff67fedba50214c
theoldnewbie <c00per@mail.ua>
2015-10-08 18:56:12 +0300

    test

---
+++ js/main.js
@@ -0,0 +1,88 @@
+$(function() {
+
+	var $searchForm = $('#search_form');
+
+	var $searchInput = $('#search');
+
+	var queryText = (function(){
+		var _value = '';
+		return {
+				get: function(){
+					return _value;
+				},
+				set: function(val){
+					_value=val;
+					$searchInput.trigger('searchText:change', val);
+				}
+		};
+
+	})();
+	
+
+	var onSubmitHandler = function(event) {
+		queryText.set($searchInput.val());
+		event.preventDefault();
+	};
+
+	var showBuildingsList = function (response) {
+		console.log(response);
+		var elements = response.listings;
+		var $divTemp = $('<div>');
+		var $result = $('#result');
+		var addHtml = function(element){
+			$divTemp.append('<form> <img src =' + element.img_url + '>');
+			$divTemp.append('<br>'+element.price_formatted);
+			$divTemp.append('<br>'+element.title);
+			$divTemp.append('<br>'+element.summary+'</form>');
+		};
+		elements.forEach(addHtml);
+
+		$result.html($divTemp);
+	};
+	var showLocationsList = function (response) {
+		console.log(response);
+		var elements = response.locations;
+		var $divTemp = $('<div>');
+		var $result = $('#result');
+		var addHtml = function(element){
+			$divTemp.append('<li>'+element.long_title+'</li>');
+		};
+		elements.forEach(addHtml);
+
+		$result.html('Возможно вы имелли ввиду:').append($divTemp);;
+
+	};
+	var showErrorList = function () {
+		$('#result').html('<h1> <span style="color:#FF0000"> Оуу, что то пошло не так! </h1>')
+
+	};
+
+	var onChangeQueryText = function (event, text) {
+		$searchInput.val('');
+		var callbackSuccess = function (data){
+			//console.info(data);
+			switch(data.response.application_response_code) {
+				case '100':
+				case '101':
+				case '110':
+					showBuildingsList(data.response);
+				break;
+				case '200':
+				case '202':
+					showLocationsList(data.response);
+				break;
+				default:
+					showErrorList(data.response);
+			}
+		};
+		var callbackError = function (data) {
+			console.error(data);
+			showErrorList(data);
+		};
+		nestoriaApi.getLocations(text, callbackSuccess, callbackError);
+	};
+
+	$searchForm.on('submit', onSubmitHandler);
+
+	$searchInput.on('searchText:change', onChangeQueryText);
+});
---
+++ js/nestoria.js
@@ -0,0 +1,23 @@
+(function(global) {
+
+	var url = 'http://api.nestoria.co.uk/api'+
+		'?country=uk&pretty=1&action=search_listings&encoding='+
+		'json&listing_type=buy&page=1'+
+		'&number_of_results=6';
+
+	var nestoriaApi ={
+		getLocations: function(placeName, successCallback, errorCallback){
+
+			$.ajax({
+				url: url+'&place_name='+placeName,
+				method: 'GET',
+				timeout: 5000,
+				dataType: "jsonp",
+				success: successCallback,
+				error: errorCallback,
+			});
+		},
+
+	};
+	global.nestoriaApi = nestoriaApi;
+})(window);
